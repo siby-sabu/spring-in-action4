19ce49086a388dd4501d91deae52cc1c590b1a74
i336545 <siby.sabu@sap.com>
2020-03-22 14:26:11 +0530

    removed aws credentials

--- Spittr/src/com/spittr/SpitterConstants.java
+++ Spittr/src/com/spittr/SpitterConstants.java
@@ -2,8 +2,8 @@ package com.spittr;
 
 public class SpitterConstants {
 	
-	public static final String awsAccessKeyId = "AKIAI5TNYZXL5HPXV2XA";
-	public static final String awsSecretKey = "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";
+	public static final String awsAccessKeyId = "swfssffs";
+	public static final String awsSecretKey = "HSrjfaS/sfsfsfs";
 	public static final String awsBucketName = "sibysabu-springinaction";
 
 }
