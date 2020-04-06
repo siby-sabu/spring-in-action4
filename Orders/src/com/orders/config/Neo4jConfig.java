a42881fb2b2623727700d18bae09042c32885835
i336545 <siby.sabu@sap.com>
2020-04-06 14:32:11 +0530

    neo4j db changes. Not completed

---
+++ Orders/src/com/orders/config/Neo4jConfig.java
@@ -0,0 +1,10 @@
+package com.orders.config;
+
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+@EnableNeo4jRepositories(basePackages = "com.orders")
+public class Neo4jConfig  {
+	
+
+
+}
